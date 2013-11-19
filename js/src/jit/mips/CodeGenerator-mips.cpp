36e9ca7532ad4f86a3374c59917b32479453c018
wangqing <wangqing-hf@loongson.cn>
2013-11-19 13:18:55 +0800

    add flags for neg32 to deal with overflow in jit/CodeGenerator.cpp

--- js/src/jit/mips/CodeGenerator-mips.cpp
+++ js/src/jit/mips/CodeGenerator-mips.cpp
@@ -537,14 +537,13 @@ CodeGeneratorMIPS::visitAbsD(LAbsD *ins)
 {
     FloatRegister input = ToFloatRegister(ins->input());
     JS_ASSERT(input == ToFloatRegister(ins->output()));
-/*    //masm.xorpd(ScratchFloatReg, ScratchFloatReg);
+    //masm.xorpd(ScratchFloatReg, ScratchFloatReg);
     //by weizhenwei, 2013.11.08
-    masm.zerod(ScratchFloatReg);
-    masm.subsd(input, ScratchFloatReg); // negate the sign bit.
-    masm.andpd(ScratchFloatReg, input); // s & ~s
-  */
-  masm.absd(input, input);
-  return true;
+//    masm.zerod(ScratchFloatReg);
+//    masm.subsd(input, ScratchFloatReg); // negate the sign bit.
+//    masm.andpd(ScratchFloatReg, input); // s & ~s
+	masm.absd(input, input);
+    return true;
 }
 
 bool
